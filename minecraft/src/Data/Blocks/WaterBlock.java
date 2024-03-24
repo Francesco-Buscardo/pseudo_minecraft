a851868e994e09e7d969f6da1e3bddaa9ee1c625
Francesco-Buscardo <147239578+Francesco-Buscardo@users.noreply.github.com>
2024-03-24 11:48:49 +0100

    Update WaterBlock.java

--- minecraft/src/Data/Blocks/WaterBlock.java
+++ minecraft/src/Data/Blocks/WaterBlock.java
@@ -6,4 +6,21 @@ public class WaterBlock extends AbstractBlock {
         this.fall_through       = true;
         this.falls_with_gravity = true;
     };
+    ////////////////////////////////////////////////////
+    @Override
+    public void setFall_through (boolean x) {
+        this.fall_through = x;
+    };
+    @Override
+    public boolean getFall_through () {
+        return this.fall_through;
+    };
+    @Override
+    public void setFalls_with_gravity (boolean x) {
+        this.falls_with_gravity = x;
+    };
+    @Override
+    public boolean getFalls_with_gravity () {
+        return this.falls_with_gravity;
+    };
 }
