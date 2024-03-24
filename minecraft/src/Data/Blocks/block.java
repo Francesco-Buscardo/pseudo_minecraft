6620d63677140017e3c424d715b8a78aab6f4f22
Francesco-Buscardo <147239578+Francesco-Buscardo@users.noreply.github.com>
2024-03-24 11:48:16 +0100

    Update block.java

--- minecraft/src/Data/Blocks/block.java
+++ minecraft/src/Data/Blocks/block.java
@@ -5,6 +5,9 @@ public interface block {
     boolean getFall_through ();
     boolean getFalls_with_gravity ();
     char display ();
+    ////////////////////////////////////////////////////
+    void setFall_through (boolean x);
+    void setFalls_with_gravity (boolean x);
 }
  // se facessi block A = new sottoclasse di AbstractBlock
  // allora A puo usare solo i metodi definiti nell'interfaccia block
