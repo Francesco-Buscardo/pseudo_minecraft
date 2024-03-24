b861dd38e08310c6f36407a4cd38ebf09510b701
Francesco-Buscardo <147239578+Francesco-Buscardo@users.noreply.github.com>
2024-03-24 11:49:55 +0100

    Update AirBlock.java

--- minecraft/src/Data/Blocks/AirBlock.java
+++ minecraft/src/Data/Blocks/AirBlock.java
@@ -11,4 +11,21 @@ public class AirBlock extends AbstractBlock{
         this.fall_through       = f_t;
         this.falls_with_gravity = f_w;
     };
+    ////////////////////////////////////////////////////
+    @Override
+    public void setFall_through (boolean x) {
+        this.fall_through = x;
+    };
+    @Override
+    public boolean getFall_through () {
+        return this.fall_through;
+    };
+    @Override
+    public void setFalls_with_gravity (boolean x) {
+        this.falls_with_gravity = x;
+    };
+    @Override
+    public boolean getFalls_with_gravity () {
+        return this.falls_with_gravity;
+    };
 }
