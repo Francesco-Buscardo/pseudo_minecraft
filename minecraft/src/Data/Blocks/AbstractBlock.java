b48d231999a77617e3702cc7d1aed7b53c96f133
Francesco-Buscardo <147239578+Francesco-Buscardo@users.noreply.github.com>
2024-03-24 11:47:26 +0100

    Update AbstractBlock.java

--- minecraft/src/Data/Blocks/AbstractBlock.java
+++ minecraft/src/Data/Blocks/AbstractBlock.java
@@ -1,24 +1,23 @@
 package Data.Blocks;
 
-public class AbstractBlock implements block {
+abstract public class AbstractBlock implements block {
      char    contenuto;
      boolean falls_with_gravity,
              fall_through;
-    //cacca molla
     ///////////////////////////////////////////////
     // GETTER SETTER ------------------------------
-    public void setFall_through (boolean x) {          //setter getter servono per settare ed ridare il valore
-        this.fall_through = x;                         //delle variabili che sono private
-    };
-    public void setFalls_with_gravity (boolean x) {
+    abstract public void setFall_through (boolean x) ; /*{
+        this.fall_through = x;
+    };*/
+    abstract public void setFalls_with_gravity (boolean x); /*{
         this.falls_with_gravity = x;
-    };
-    public boolean getFall_through () {
+    };*/
+    abstract public boolean getFall_through () ; /* {
         return this.fall_through;
-    };
-    public boolean getFalls_with_gravity () {
+    };*/
+    abstract public boolean getFalls_with_gravity ();/* {
         return this.falls_with_gravity;
-    };
+    };*/
     ///////////////////////////////////////////////
     // COSTRUCTORS --------------------------------
     // SONO NEI FIGLI CHE SAREBBE I VARI TIPI DI BLOCCHI
